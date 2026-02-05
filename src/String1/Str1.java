6217f459a1946b58c83636adedea15650eabb435
Guma-tgs <gulmiratuleberdieva@gmail.com>
2026-02-05 12:43:30 +0600

    Initial commit

---
+++ src/String1/Str1.java
@@ -0,0 +1,14 @@
+package String1;
+
+import java.util.Scanner;
+
+public class Str1 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String str2 = scan.nextLine();
+        System.out.println("Hellо " + str2 +"!");
+
+    }
+
+}
